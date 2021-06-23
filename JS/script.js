9f37e36af5f91bdb7783a818d8c9e38372d77b68
Beni1212 <78383896+Beni1212@users.noreply.github.com>
2021-06-23 10:14:03 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,81 @@
+var hamburger = document.querySelector(".hamburger");
+	hamburger.addEventListener("click", function(){
+		document.querySelector("body").classList.toggle("active");
+	})
+
+
+
+
+
+
+
+/* button */
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+
+/* end button */
+
+
+
+
+
+function check() {
+  var checkBox = document.getElementById("checbox");
+  var text1 = document.getElementsByClassName("text1");
+  var text2 = document.getElementsByClassName("text2");
+
+  for (var i = 0; i < text1.length; i++) {
+    if (checkBox.checked == true) {
+      text1[i].style.display = "block";
+      text2[i].style.display = "none";
+    } else if (checkBox.checked == false) {
+      text1[i].style.display = "none";
+      text2[i].style.display = "block";
+    }
+  }
+}
+check();
+
+
+
+
+
+
+$(function() {
+  $('.pop-up').hide();
+  $('.pop-up').fadeIn(1000);
+  
+      $('.close-button').click(function (e) { 
+
+      $('.pop-up').fadeOut(700);
+      $('#overlay').removeClass('blur-in');
+      $('#overlay').addClass('blur-out');
+      e.stopPropagation();
+        
+    });
+ });
+
+
+
+
+
+
+
+
 
